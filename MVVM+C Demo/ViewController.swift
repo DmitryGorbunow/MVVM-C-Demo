a3a7a47664c1cb9bc02ef740707ce8222a276146
DmitryGorbunow <dmitry.gorbunow@gmail.com>
2023-10-29 14:45:43 +0500

    Initial Commit

---
+++ MVVM+C Demo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MVVM+C Demo
+//
+//  Created by Dmitry Gorbunow on 10/29/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
